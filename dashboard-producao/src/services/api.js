8223f922e1f934bd27a7058531b5da3d3a5260a3
LucasMartins <LucasMartins@GO-3D-AAD-7>
2026-02-24 11:41:03 -0300

    add link service

--- dashboard-producao/src/services/api.js
+++ dashboard-producao/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://localhost:7248/api';
+const API_BASE_URL = 'https://back-endsystem3d.onrender.com';
 
 const api = axios.create({
  baseURL: API_BASE_URL,
